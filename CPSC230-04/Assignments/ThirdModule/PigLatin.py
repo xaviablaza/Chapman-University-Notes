c9e51bee370f21efa4bd86ecafe1a429d12061bf
Xavi Ablaza <xlablaza@gmail.com>
2015-10-24 14:59:31 -0700

    Added newline handling for Python assignment 2

--- CPSC230-04/Assignments/ThirdModule/PigLatin.py
+++ CPSC230-04/Assignments/ThirdModule/PigLatin.py
@@ -35,40 +35,52 @@ def printLineAndWordCounts():
     print "A total of " + str(line_count) + " lines were translated successfully."
     print "A total of " + str(word_count) + " words were translated successfully."
 
-# Ask for path to file to be translated
-path = raw_input("Enter the path of a text file: ")
 
-# Open input file (file to be translated)
-input_file = open(path, "r")
-
-# Create output file
-output_file = open("pig_output.txt", "w")
-
-output_lines = ""
-# Iterate through input file
-for line in input_file:
+while True:
+    # Ask for path to file to be translated
+    path = raw_input("Enter the path of a text file: ")
     
-    # Strip newline character
-    line = line.strip("\n")
-    # you need to assign the line :/
+    try:
+        # Open input file (file to be translated)
+        input_file = open(path, "r")
+    except IOError:
+        print path + " does not exist!"
+        continue
     
-    # Create list of words on line via split
-    words = line.split(" ")
+    # Create output file
+    output_file = open("pig_output.txt", "w")
     
-    # Declare new empty translated_line
-    translated_line = ""
-    translated_word = ""
+    output_lines = ""
+    # Iterate through input file
+    for line in input_file:
+        line_count+=1
+        
+        # Handle blank line
+        if line == "\n":
+            output_lines += "\n"
+            continue
+        
+        # Strip newline character and other whitespace characters
+        line = line.strip(" \t\n\r")    
+        # you need to assign the line :/
+        
+        # Create list of words on line via split
+        words = line.split(" ")
+        
+        # Declare new empty translated_line
+        translated_line = ""
+        translated_word = ""
+            
+        # Translate each word
+        for word in words:
+            translated_word = translateToPigLatin(word)
+            word_count+=1
+            translated_line+=translated_word + " "
+        
+        output_lines += translated_line[:-1]+"\n"
     
-    # Translate each word
-    for word in words:
-        translated_word = translateToPigLatin(word)
-        word_count+=1
-        translated_line+=translated_word + " "
-    
-    output_lines += translated_line[:-1]+"\n"
-    line_count+=1
-
-output_file.write(output_lines[:-2])
-input_file.close()
-output_file.close()
-printLineAndWordCounts()
+    output_file.write(output_lines[:-1])
+    input_file.close()
+    output_file.close()
+    printLineAndWordCounts()
+    break
