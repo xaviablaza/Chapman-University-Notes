40a9d988109ec2f340ff2d3e3c358639f7a95630
xaviablaza <xlablaza@gmail.com>
2016-04-19 23:23:09 -0700

    Added test code in main.cpp

---
+++ CPSC350 Data Structures and Algorithms/Assignment5/main.cpp
@@ -0,0 +1,44 @@
+// #include "InputHandler.h"
+#include <iostream>
+#include "BST.h"
+#include "Student.h"
+using namespace std;
+int main(int argc, char** argv) {
+	/*
+	Begin test code
+
+	BST<int> bst = BST<int>();
+	bst.insert(7);
+	bst.insert(4);
+	bst.insert(9);
+	bst.insert(5);
+	bst.insert(6);
+	bst.insert(2);
+	bst.insert(8);
+	bst.insert(3);
+	bst.insert(1);
+	bst.printTree();
+	cout<<"---END---"<<endl;
+	bst.deleteNode(7);
+	bst.printTree();
+	cout<<"---END---"<<endl;
+	bst.deleteNode(4);
+	bst.printTree();
+	cout<<"---END---"<<endl;
+	bst.deleteNode(2);
+	bst.printTree();
+	cout<<"---END---"<<endl;
+	bst.deleteNode(8);
+	bst.printTree();
+
+	Student s1 = Student();
+	s1.id = 500;
+	Student s2 = Student();
+	s2.id = 100;
+	bool result = s1>s2;
+	cout<<result<<endl;
+	
+	End test code
+	*/
+	return 0;
+}
