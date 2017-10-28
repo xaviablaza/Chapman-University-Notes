aea4ab20239dd49c5b75052c637f43715ba2c408
Xavi Ablaza <xlablaza@gmail.com>
2017-10-28 15:12:21 -0700

    Started project5

---
+++ CPSC236 Visual Programming/project5/WindowsFormsApp1/WindowsFormsApp1/Donor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Donor
+    {
+        public Donor(string firstName, string lastName, string address, string phone, DateTime birthDate, Blood blood)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            Phone = phone;
+            Birthdate = birthDate;
+            Blood = blood;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public Blood Blood { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + "," + LastName + "," + Address + "," + Phone + "," + Birthdate.ToString() + "," + Blood.ToString();
+        }
+    }
+}
---
+++ CPSC236 Visual Programming/project5/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        BindingList<Donor> donorList;
+        const string FILENAME = "donors.txt";
+
+        public Form1()
+        {
+            donorList = new BindingList<Donor>();
+            loadFile(donorList, FILENAME);
+            InitializeComponent();
+            // TODO: Bind the donor list to the listbox
+            
+        }
+
+        private void saveFile(BindingList<Donor> donors, string filename)
+        {
+            using (StreamWriter sw = new StreamWriter(filename, false))
+            {
+                foreach (Donor donor in donors)
+                {
+                    //"Firstname,LastName,address,phone,bdaydate,bloodtype,rhfactor"
+                    sw.WriteLine(donor.ToString());
+                }
+            }
+        }
+
+        private bool loadFile(BindingList<Donor> donors, string fileName)
+        {
+            // If file exists, then attempt to load it into dictionary
+            if (File.Exists(fileName))
+            {
+                try
+                {   // Open the text file using a stream reader.
+                    using (StreamReader sr = new StreamReader(fileName))
+                    {
+                        // Read the stream to a string
+                        String line = sr.ReadToEnd();
+                        String[] v = line.Split(',');
+                        //"Firstname,LastName,address,phone,bdaydate,bloodtype,rhfactor"
+                        donors.Add(new Donor(v[0], v[1], v[2], v[3], DateTime.Parse(v[4]), new Blood(v[5], v[6])));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(e.Message);
+                    return false;
+                }
+                Console.WriteLine("Successfully loaded " + fileName);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            saveFile(donorList, FILENAME);
+        }
+    }
+}
