427df79a55adbb66bf3a122d6afdce24814770b0
xaviablaza <xlablaza@gmail.com>
2017-12-07 15:41:09 -0800

    Finished my own MoBettaEngine.hs

--- CPSC354 Programming Languages/mobetta/MoBettaEngine.hs
+++ CPSC354 Programming Languages/mobetta/MoBettaEngine.hs
@@ -59,7 +59,7 @@ statementAction (While b s) = whileAction (boolCalc b) (statementAction s)
                         -- compute "while b s"
 statementAction (Assign v e) = assignAction v (intCalc e)
                         -- compute e, and assign to v
-statementAction (Block ls) = blockAction $ map statementAction ls
+statementAction (Block ls) = makeProgram ls
                         -- compute a sequence of individual computations
                         -- by translating each into a computation
 
@@ -117,8 +117,8 @@ msgAction s = doIO $ putStr s
 -- Display result of computing an integer
 printAction :: IntCalc -> Action
 printAction intCalc = do
-  n <- intCalc -- execute given integer calculation to obtain an Integer
-  doIO $ putStr $ show n -- display it
+    n <- intCalc -- n is the result of doing the integer calculation
+    doIO $ putStr (show n) -- show n converts the integer into a string representation
 
 -- Compute an integer, then store it
 assignAction :: String -> IntCalc -> Action
@@ -145,71 +145,65 @@ whileAction boolCalc action = do
       whileAction boolCalc action
 
 -- Do a list of actions sequentially.
--- NB. There is a combinator for this, but I've written it out for clarity.
 blockAction :: [Action] -> Action
 blockAction [] = return ()
 blockAction (a:ls) = do
   a
   blockAction ls
 
-{--------------------------------------------------------------------------
-Interpretations of integer and boolean expressions
----------------------------------------------------------------------------}
+-- comparator table to lookup boolean comparison operators
+compTable = [ (Greater, (>))
+            , (Less, (<))
+            , (GreaterEqual, (>=))
+            , (LessEqual, (<=))
+            , (Equal, (==))
+            , (NEqual, (/=))]
 
--- Lookup tables for the various arithmetic and Boolean operators.
---  These associate AST operators with their semantics.
--- I have written things this way to make integer and boolean calculations
--- easier to code.
+-- and or table
+andOrTable = [ (And, (&&))
+             , (Or, (||))]
 
-aBinOps =
-  [ (Add, (+))
-  , (Sub, (-))
-  , (Mul, (*))
-  , (Div, div)
-  , (Mod, mod)]
+-- unary operator table
+unaryOpTable =  [(Not, not)]
 
-aUnOps =  [(Neg, negate)]
+-- This defines the translation of a BExpr into a computation of Booleans
+boolCalc :: BExpr -> BoolCalc
+boolCalc (BoolConst b) = return b
+boolCalc (Reln cOp expr1 expr2) = do -- comparison operator
+  b1 <- (boolCalc expr1)
+  b2 <- (boolCalc expr2)
+  let func = (lookup cOp compTable)
+  return $ func b1 b2
+boolCalc (BBin op expr1 expr2) = do
+  b1 <- (boolCalc expr1)
+  b2 <- (boolCalc expr2)
+  let func = (lookup op andOrTable)
+  return $ func b1 b2
+boolCalc (BUn op expr) = do
+  r1 <- (boolCalc expr)
+  let func = (lookup op unaryOpTable)
+  return $ func r1
 
-bBinOps =
-  [ (And, (&&))
-  , (Or, (||))]
+-- used as a lookup table to find out what the operation actually does
+opTable = [ (Mul, (*))
+          , (Div, div)
+          , (Mod, mod)
+          , (Add, (+))
+          , (Sub, (-))]
 
-bUnOps =  [(Not, not)]
-
-relnOps =
-  [ (Greater, (>))
-  , (GreaterEqual, (>=))
-  , (Less, (<))
-  , (LessEqual, (<=))
-  , (Equal, (==))
-  , (NEqual, (/=))]
-
--- Find the actual operation associated with a constructor.
--- N.B. lookup returns a (Maybe ...).
--- But the only way it would return Nothing
---   is if something impossible has happened (the parser somehow produced an
---   impossible AST).
--- So it is safe to unwrap a result of the form (Just ...)
--- The combination of fromMaybe with (error ...) does that
 opLookup :: Eq const => const -> [(const, sem)] -> sem
 opLookup op opTable =
   fromMaybe (error "Undefined operator. Should never happen.")
             (lookup op opTable)
 
--- This defines the translation of a BExpr into a computation of Booleans
-boolCalc :: BExpr -> BoolCalc
-boolCalc (BoolConst b) = return b
-boolCalc (Reln cOp expr1 expr2)
-  = liftA2 (opLookup cOp relnOps) (intCalc expr1) (intCalc expr2)
-boolCalc (BBin op expr1 expr2)
-  = liftA2 (opLookup op bBinOps) (boolCalc expr1) (boolCalc expr2)
-boolCalc (BUn op expr)
-  = fmap (opLookup op bUnOps) (boolCalc expr)
-
 intCalc :: AExpr -> IntCalc
-intCalc (Var v) = retrieveEnv v
-intCalc (IntConst val) = return val
-intCalc (ABin op expr1 expr2)
-  = liftA2 (opLookup op aBinOps) (intCalc expr1) (intCalc expr2)
-intCalc (AUn op expr)
-  = fmap (opLookup op aUnOps) (intCalc expr)
+intCalc (Var v) = retrieveEnv v -- thing that calculates an integer when you ask it to (gives an integer to you when you want an integer
+intCalc (IntConst val) = return val -- if you ask me to do a computation then it would return that particular value
+intCalc (ABin op expr1 expr2) = do
+  result1 <- (intCalc expr1) -- result of first expression
+  result2 <- (intCalc expr2) -- result of second expression
+  let func = (opLookup op opTable) -- lookup to see if the operator is valid using opLookup helper function
+  return $ func result1 result2 -- return everything
+intCalc (AUn op expr) = do
+  n <- expr
+  return $ negate n
