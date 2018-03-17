20c72b368bc1cb9e06894bd15e132561036297e7
Xavi Ablaza <xlablaza@gmail.com>
2018-03-16 23:23:04 -0700

    Added google maps html/js

---
+++ SE320 Software Development Lifecycle/js/map_api.js
@@ -0,0 +1,117 @@
+
+
+function radiusParam(map){ //RADIUS FOR PSAFE TO PICK UP
+var rectangle;
+
+var bounds = { //PARAMS OF PSAFE TO ALLOW PICKUP
+          north: 33.805033545182255,
+          south: 33.776082751851,
+          east: -117.83182508881,
+          west: -117.8779956515
+        };
+        // Define the rectangle and set its editable property to true.
+        rectangle = new google.maps.Rectangle({
+          bounds: bounds,
+          editable: true,
+          draggable: false
+        });
+  rectangle.setMap(map);
+}
+
+
+function nightMode(time, theMap){ //NIGHT MODE FEATURE
+
+  if (time >= 18 || time <= 7) { //if the time is around 6PM change to Night Mode
+  // Styles a map in night mode.
+         theMap = new google.maps.Map(document.getElementById('map'), {
+           center: {lat: 33.793348, lng: -117.851350},
+           zoom: 13,
+           styles: [
+             {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+             {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+             {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+             {
+               featureType: 'administrative.locality',
+               elementType: 'labels.text.fill',
+               stylers: [{color: '#d59563'}]
+             },
+             {
+               featureType: 'poi',
+               elementType: 'labels.text.fill',
+               stylers: [{color: '#d59563'}]
+             },
+             {
+               featureType: 'poi.park',
+               elementType: 'geometry',
+               stylers: [{color: '#263c3f'}]
+             },
+             {
+               featureType: 'poi.park',
+               elementType: 'labels.text.fill',
+               stylers: [{color: '#6b9a76'}]
+             },
+             {
+               featureType: 'road',
+               elementType: 'geometry',
+               stylers: [{color: '#38414e'}]
+             },
+             {
+               featureType: 'road',
+               elementType: 'geometry.stroke',
+               stylers: [{color: '#212a37'}]
+             },
+             {
+               featureType: 'road',
+               elementType: 'labels.text.fill',
+               stylers: [{color: '#9ca5b3'}]
+             },
+             {
+               featureType: 'road.highway',
+               elementType: 'geometry',
+               stylers: [{color: '#746855'}]
+             },
+             {
+               featureType: 'road.highway',
+               elementType: 'geometry.stroke',
+               stylers: [{color: '#1f2835'}]
+             },
+             {
+               featureType: 'road.highway',
+               elementType: 'labels.text.fill',
+               stylers: [{color: '#f3d19c'}]
+             },
+             {
+               featureType: 'transit',
+               elementType: 'geometry',
+               stylers: [{color: '#2f3948'}]
+             },
+             {
+               featureType: 'transit.station',
+               elementType: 'labels.text.fill',
+               stylers: [{color: '#d59563'}]
+             },
+             {
+               featureType: 'water',
+               elementType: 'geometry',
+               stylers: [{color: '#17263c'}]
+             },
+             {
+               featureType: 'water',
+               elementType: 'labels.text.fill',
+               stylers: [{color: '#515c6d'}]
+             },
+             {
+               featureType: 'water',
+               elementType: 'labels.text.stroke',
+               stylers: [{color: '#17263c'}]
+             }
+           ]
+         });
+
+       } else {
+         theMap = theMap; //it is not dark
+      }
+
+    radiusParam(theMap);
+    return theMap;
+}
